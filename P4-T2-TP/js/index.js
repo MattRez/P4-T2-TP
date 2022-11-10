6a411b3fe8f60c1a565ac55ec1fbf8d283201985
Matheus Rezende <102304681+MattRez@users.noreply.github.com>
2022-11-10 17:22:01 -0300

    Add files via upload

--- P4-T2-TP/js/index.js
+++ P4-T2-TP/js/index.js
@@ -1,13 +1,51 @@
-const start = () => {
+const start_olc = () => {
 
 }
 
+start_old()
+
+// abc123
+
+const start = () => {
+    // const 
+
+    const addButton = document.getElementById('addButton')
+    addButton.onclick = createNewGuest
+}
+
+const createNewGuest = () => {
+    const inputGuestName = document.getElementById('guestName')
+    const newItem = createListItem(inputGuestName.value)
+    const list = document.getElementById('guestList')
+    list.appendChild(newItem)
+
+    clearAndFocusInputElement(inputGuestName)
+}
+
+const createListItem = contentListItem => {
+    const newItem = document.createElement('li')
+    const itemTextNode = document.createTextNode(contentListItem)
+    newItem.appendChild(itemTextNode)
+
+    const deleteButton = document.createElement('input')
+    deleteButton.type = 'button'
+    deleteButton.value = 'Remover'
+    deleteButton.onclick = deleteListItemEvent
+    newItem.appendChild(deleteButton)
+
+    return newItem
+}
+
+const clearAndFocusInputElement = input => {
+    input.value = ''
+    input.focus()
+}
+
+const deleteListItemEvent = event => {
+    const input = event.target
+    const listItem = input.parentNode
+    const list = listItem.parentNode
+    list.removeChild(listItem)
+}
+
 start()
-
-
-
-
-
-
-
-
